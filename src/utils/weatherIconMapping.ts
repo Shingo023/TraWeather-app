df272c714ca332e515f72976266c8abfbbcd9c3a
Shingo023 <s22sugi@gmail.com>
2024-12-12 14:40:33 -0800

    Update weather-related images.

--- src/utils/weatherIconMapping.ts
+++ src/utils/weatherIconMapping.ts
@@ -1,25 +1,23 @@
 export const iconMapping = {
-  "clear-day": "/weather-icons/clear-day.png",
-  "clear-night": "/weather-icons/clear-night.png",
-  rain: "/weather-icons/rain.png",
-  snow: "/weather-icons/snow.png",
-  wind: "/weather-icons/wind.png",
-  fog: "/weather-icons/fog.png",
-  cloudy: "/weather-icons/cloudy.png",
-  "partly-cloudy-day": "/weather-icons/partly-cloudy-day.png",
-  "partly-cloudy-night": "/weather-icons/partly-cloudy-night.png",
+  "clear-day": "/weather-icons/clear-day.svg",
+  "clear-night": "/weather-icons/clear-night.svg",
+  rain: "/weather-icons/rain.svg",
+  snow: "/weather-icons/snow.svg",
+  wind: "/weather-icons/wind.svg",
+  fog: "/weather-icons/fog.svg",
+  cloudy: "/weather-icons/cloudy.svg",
+  "partly-cloudy-day": "/weather-icons/partly-cloudy-day.svg",
+  "partly-cloudy-night": "/weather-icons/partly-cloudy-night.svg",
 };
 
 export const backgroundMapping = {
-  "clear-day": "/backgrounds/clear-day-bg.png",
-  rain: "/backgrounds/rain-bg.png",
-  cloudy: "/backgrounds/cloudy-bg.png",
-
-  // need to be changed
-  "clear-night": "/backgrounds/clear-day-bg.png",
-  snow: "/backgrounds/cloudy-bg.png",
-  wind: "/backgrounds/cloudy-bg.png",
-  fog: "/backgrounds/cloudy-bg.png",
-  "partly-cloudy-day": "/backgrounds/clear-day-bg.png",
-  "partly-cloudy-night": "/backgrounds/clear-day-bg.png",
+  "clear-day": "/backgrounds/clear-day-bg.svg",
+  rain: "/backgrounds/rain-bg.svg",
+  cloudy: "/backgrounds/cloudy-bg.svg",
+  "clear-night": "/backgrounds/clear-night-bg.svg",
+  snow: "/backgrounds/snow-bg.svg",
+  wind: "/backgrounds/wind-bg.svg",
+  fog: "/backgrounds/fog-bg.svg",
+  "partly-cloudy-day": "/backgrounds/partly-cloudy-day-bg.svg",
+  "partly-cloudy-night": "/backgrounds/partly-cloudy-night-bg.svg",
 };
